77c6377e0b5192b0dc8b1e68eba74925afea690f
pegdwende1 <136675538+pegdwende1@users.noreply.github.com>
2024-04-14 19:59:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_s3_bucket" "example" {
 
 
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-0b990d3cfca306617"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
@@ -84,7 +84,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-0b990d3cfca306617"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub2.id
